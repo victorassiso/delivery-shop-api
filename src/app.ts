1893a4e268c016643c8f19fb1b3e0517dcd3ef9c
Victor Assis <victor_assis@poli.ufrj.br>
2024-01-12 23:50:32 -0300

    ADD: Fastify Cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
 
-import { prisma } from './lib/prisma'
 import { Router } from './router'
 
 export const app = fastify()
+
+app.register(cors, {})
 
 app.register(Router)
