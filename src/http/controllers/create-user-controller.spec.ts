0499288a11c3fb17d42f383e87466e1eebff8fd7
Victor Assis <victor_assis@poli.ufrj.br>
2024-01-13 23:59:52 -0300

    ADD: Create User E2E Test

--- src/http/controllers/create-user-controller.spec.ts
+++ src/http/controllers/create-user-controller.spec.ts
@@ -1,4 +1,20 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { beforeAll, describe, expect, it } from 'vitest'
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-test('ok', () => {})
+import { app } from '@/app'
+
+describe('Create User (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  it('should be able to create a new user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
