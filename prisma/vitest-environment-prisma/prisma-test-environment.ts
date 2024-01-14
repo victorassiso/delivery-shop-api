393eaba85b6b92f0b826ff4bc6ee0685603c4664
Victor Assis <victor_assis@poli.ufrj.br>
2024-01-13 23:23:42 -0300

    ADD: Creating Test Environment

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,15 @@
+import { Environment } from 'vitest'
+
+export default <Environment>{
+  name: 'prisma',
+  async setup() {
+    console.log('Executou')
+
+    return {
+      async teardown() {
+        console.log('Tear')
+      },
+    }
+  },
+  transformMode: 'web',
+}
