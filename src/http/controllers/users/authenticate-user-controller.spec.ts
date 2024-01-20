77c968abfb5166bcb81d1e0fcb37ae2215d6a220
Victor Assis <victor_assis@poli.ufrj.br>
2024-01-20 14:28:00 -0300

    FEAT: Test: Should not be able to authenticate with wrong credentials

--- src/http/controllers/users/authenticate-user-controller.spec.ts
+++ src/http/controllers/users/authenticate-user-controller.spec.ts
@@ -25,4 +25,20 @@ describe('Authenticate User (e2e)', () => {
       token: expect.any(String),
     })
   })
+
+  it('should not be able to authenticate an user using wrong credentials', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'wrong@example.com',
+      password: 'wrong123',
+    })
+
+    expect(response.statusCode).toEqual(400)
+    expect(response.body?.message).toEqual('Invalid credentials')
+  })
 })
